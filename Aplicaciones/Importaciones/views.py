e10175b3f2d267526a21c83658af774c9d1e7183
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-01 16:30:34 -0500

    added views listas

--- Aplicaciones/Importaciones/views.py
+++ Aplicaciones/Importaciones/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cliente, Proveedor, Catalogo, Pedido, Producto
 
 # Create your views here.
 def plantilla(request):
     return render(request, 'plantilla.html')
+
+#Listar datos de la tabla Cliente
+def listaClientes(request):
+    clienteBdd=Cliente.objects.all()
+    return render(request, 'listaClientes.html', {'clientes':clienteBdd})
+
+#Lsitar datos de la tabla Proveedor
+def listaProveedores(request):
+    proveedorBdd=Proveedor.objects.all()
+    return render(request, 'listaProveedores.html', {'proveedores':proveedorBdd})
+
+#Listar datos de la tabla Catalogo
+def listaCatalogos(request):
+    catalogoBdd=Catalogo.objects.all()
+    return render(request, 'listaCatalogos.html', {'catalogos':catalogoBdd})
