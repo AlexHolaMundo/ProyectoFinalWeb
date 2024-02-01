b33f1c56670a26ead3ff77fea681b8a8f6b7c1f8
LizzyJoss <joselinespin01@gmail.com>
2024-02-01 15:52:45 -0500

    add model proveedor y catalogo

--- Aplicaciones/Importaciones/models.py
+++ Aplicaciones/Importaciones/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    idProveedor=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    email=models.CharField(max_length=150)
+    pais=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=50)
+    logo=models.FileField(upload_to='proveedores', null=True, blank=True)
+
+    def __str__(self):
+        fila='{1} {2} {3} {4} {5} {6}'
+        return fila.format(self.idProveedor, self.nombre, self.email, self.pais, self.ciudad, self.logo)
+
+class Catalogo(models.Model):
+    idCatalogo=models.AutoField(primary_key=True)
+    categoria=models.CharField(max_length=50)
+    descripcion=models.CharFiled(max_length=150)
+
+    def __str__(self):
+        fila='{1} {2} {3}'
+        return fila.format(self.idCatalogo, self.categoria, self.descripcion)
