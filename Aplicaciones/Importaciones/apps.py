42093d8e3bd1023cf3da7d3fd2512e95787a628a
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-01 14:57:47 -0500

    added theme

--- Aplicaciones/Importaciones/apps.py
+++ Aplicaciones/Importaciones/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ImportacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Importaciones'
+    name = 'Aplicaciones.Importaciones'
--- ZenImport/settings.py
+++ ZenImport/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for ZenImport project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aplicaciones.Importaciones'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'ZenImport.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'importaciones.db',
     }
 }
 
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
@@ -115,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'ZenImport/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+MEDIA_URL='/media/'
+MEDIA_ROOT=(os.path.join(BASE_DIR,'ZenImport/media/'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
