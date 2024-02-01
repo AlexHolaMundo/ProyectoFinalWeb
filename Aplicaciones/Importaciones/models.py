3ff85b2c81b9c7cb215265db062931f2b2508dc1
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-01 15:54:55 -0500

    add model cliente

--- Aplicaciones/Importaciones/models.py
+++ Aplicaciones/Importaciones/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    idCliente = models.AutoField(primary_key=True)
+    cedula = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        fila="{0} => {1} => {2} => {3} => {4} => {5}"
+        return fila.format(self.idCliente, self.cedula, self.nombre, self.direccion, self.email, self.telefono)
