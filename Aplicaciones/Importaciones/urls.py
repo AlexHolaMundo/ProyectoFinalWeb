f329fbabf03949e732c85229bdac13a3c7f3fd29
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-02 13:16:18 -0500

    feat add all templates 0

--- Aplicaciones/Importaciones/urls.py
+++ Aplicaciones/Importaciones/urls.py
@@ -3,4 +3,9 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('listaClientes/', views.listaClientes),
+    path('listaProveedores/', views.listaProveedores),
+    path('listaCatalogos/', views.listaCatalogos),
+    path('listaPedidos/', views.listaPedidos),
+    path('listaProductos/', views.listaProductos),
+    path('listaDetalles/', views.listaDetalles),
     ]
