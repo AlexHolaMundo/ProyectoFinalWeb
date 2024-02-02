b576272be6b51591a4214931903654b4c825c079
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-02 13:03:04 -0500

    add template listaClientes

--- Aplicaciones/Importaciones/urls.py
+++ Aplicaciones/Importaciones/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns = [path('', views.home),]
+urlpatterns = [
+    path('', views.home),
+    path('listaClientes/', views.listaClientes),
+    ]
