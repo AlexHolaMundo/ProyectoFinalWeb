edf241fa511c7310d398a56b4700f44f84530b52
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-02-02 13:06:43 -0500

    register models in admin

--- Aplicaciones/Importaciones/admin.py
+++ Aplicaciones/Importaciones/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Cliente, Proveedor, Catalogo, Pedido, Producto, Detalle
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Proveedor)
+admin.site.register(Catalogo)
+admin.site.register(Pedido)
+admin.site.register(Producto)
+admin.site.register(Detalle)
